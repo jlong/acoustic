190d431ec76164b78d0a93cdee9666a6c91d6db7
John W. Long <me@johnwlong.com>
2009-05-15 22:28:34 -0400

    a start on the spec for Acoustic::Configuration

--- spec/acoustic/configuration_spec.rb
+++ spec/acoustic/configuration_spec.rb
@@ -2,7 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'acoustic/configuration'
 
 describe Acoustic::Configuration do
-  it "should pass" do
-    true.should == true
+  it "load basic configuration" do
+    config = Acoustic::Configuration.new
+    config.load(FIXTURES_ROOT + '/configuration/basic.rb')
+    config[:answer].should == 42
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,5 @@
-$LOAD_PATH << File.expand_path(File.dirname(__FILE__) + '/../lib')
+SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
+FIXTURES_ROOT = File.expand_path(SPEC_ROOT + '/fixtures')
+LIB_ROOT = File.expand_path(SPEC_ROOT + '/../lib')
+
+$LOAD_PATH << LIB_ROOT
