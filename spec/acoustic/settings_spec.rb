024809348c5245dc07592b2638b28575447dcb47
John W. Long <me@johnwlong.com>
2009-11-20 09:59:27 -0800

    tweaked settings_spec.rb

--- spec/acoustic/settings_spec.rb
+++ spec/acoustic/settings_spec.rb
@@ -6,25 +6,27 @@ describe Acoustic::Settings do
     load :empty
   end
   
-  it 'should load gems' do
-    Kernel.should_receive(:require).with("rubygems")
-    Kernel.should_receive(:gem).with("simple")
-    Kernel.should_receive(:require).with("simple")
-    load :simple_gem_call
-  end
-  
-  it 'should load gem with version' do
-    Kernel.should_receive(:require).with("rubygems")
-    Kernel.should_receive(:gem).with("versioned", "1.2.3")
-    Kernel.should_receive(:require).with("versioned")
-    load :gem_call_with_version
-  end
-  
-  it 'should load gem with multiple version requirements' do
-    Kernel.should_receive(:require).with("rubygems")
-    Kernel.should_receive(:gem).with("versioned", "1.2.3", "1.2.5")
-    Kernel.should_receive(:require).with("versioned")
-    load :gem_call_with_multiple_versions
+  describe '#gem' do
+    it 'should load a gem and require it' do
+      Kernel.should_receive(:require).with("rubygems")
+      Kernel.should_receive(:gem).with("simple")
+      Kernel.should_receive(:require).with("simple")
+      load :simple_gem_call
+    end
+    
+    it 'should load a gem with a version requirement' do
+      Kernel.should_receive(:require).with("rubygems")
+      Kernel.should_receive(:gem).with("versioned", "1.2.3")
+      Kernel.should_receive(:require).with("versioned")
+      load :gem_call_with_version
+    end
+    
+    it 'should load a gem with multiple version requirements' do
+      Kernel.should_receive(:require).with("rubygems")
+      Kernel.should_receive(:gem).with("versioned", "1.2.3", "1.2.5")
+      Kernel.should_receive(:require).with("versioned")
+      load :gem_call_with_multiple_versions
+    end
   end
   
   def load(symbol)
